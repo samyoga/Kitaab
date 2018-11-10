c537ff098ada7a6cf0ca92f4f87d89c0777def88
samyoga <bhattaraisamyoga@gmail.com>
2018-11-10 18:27:09 +0545

    Login page created

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res){
+  res.render('login');
+});
+
+
 module.exports = router;
